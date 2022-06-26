95662f0b94ab03031ab809a77e2848aaea478be2
JuhaszZoltanAntal <48652435+JuhaszZoltanAntal@users.noreply.github.com>
2022-06-26 14:57:22 +0200

    Implement Shortcuts and Actions for keyboard events and fix dialog error

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import '../widget/search_dialog.dart';
 
+class OpenDialogIntent extends Intent {}
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -12,51 +14,39 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<LogicalKeyboardKey> keys = [];
-  late bool alreadyOpened = false;
 
   @override
   Widget build(BuildContext context) {
-    return RawKeyboardListener(
-      autofocus: true,
-      focusNode: FocusNode(),
-      onKey: (event) {
-        final key = event.logicalKey;
-        if (event is RawKeyDownEvent) {
-          if (keys.contains(key)) return;
-          setState(() => keys.add(key));
-          if (keys.contains(LogicalKeyboardKey.keyK) &&
-                  keys.contains(LogicalKeyboardKey.controlLeft) ||
-              keys.contains(LogicalKeyboardKey.keyK) &&
-                  keys.contains(LogicalKeyboardKey.controlRight)) {
-            if (!alreadyOpened) {
-              showDialog(
-                  context: context,
-                  builder: (context) {
-                    return const SearchDialog();
-                  });
-              setState(() {
-                alreadyOpened = true;
-              });
-            } else {
-              Navigator.of(context, rootNavigator: true).maybePop(context);
-              setState(() {
-                alreadyOpened = false;
-              });
-            }
-          }
-        } else {
-          setState(() => keys.remove(key));
-        }
+    return Shortcuts(
+      shortcuts: <LogicalKeySet, Intent>{
+        LogicalKeySet(LogicalKeyboardKey.control, LogicalKeyboardKey.keyK):
+            OpenDialogIntent(),
       },
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).primaryColor,
-          actions: const [
-            SearchButton(),
-          ],
-        ),
-        body: Container(
-          color: const Color(0xff2b3137),
+      child: Actions(
+        actions: <Type, Action<Intent>>{
+          OpenDialogIntent:
+              CallbackAction<OpenDialogIntent>(onInvoke: (intent) {
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return const SearchDialog();
+                });
+            return null;
+          }),
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).primaryColor,
+            actions: const [
+              SearchButton(),
+            ],
+          ),
+          body: Focus(
+            autofocus: true,
+            child: Container(
+              color: const Color(0xff2b3137),
+            ),
+          ),
         ),
       ),
     );
--- lib/widget/search_dialog.dart
+++ lib/widget/search_dialog.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+class CloseDialogIntent extends Intent {}
+
 class SearchDialog extends StatefulWidget {
   const SearchDialog({Key? key}) : super(key: key);
 
@@ -10,7 +12,6 @@ class SearchDialog extends StatefulWidget {
 
 class _SearchDialogState extends State<SearchDialog> {
   final myController = TextEditingController();
-  final keyboardFocusNode = FocusNode();
   final textFieldFocusNode = FocusNode();
 
   @override
@@ -23,187 +24,198 @@ class _SearchDialogState extends State<SearchDialog> {
   Widget build(BuildContext context) {
     final currentWidth = MediaQuery.of(context).size.width;
 
-    return RawKeyboardListener(
-      autofocus: false,
-      focusNode: keyboardFocusNode,
-      onKey: (event) {
-        if (mounted) {
-          if (event is RawKeyDownEvent) {
-            if (event.isKeyPressed(LogicalKeyboardKey.escape)) {
-              Navigator.of(context, rootNavigator: true).maybePop();
-            }
-          }
-        }
+    return Shortcuts(
+      shortcuts: <LogicalKeySet, Intent>{
+        LogicalKeySet(LogicalKeyboardKey.control, LogicalKeyboardKey.keyK):
+            CloseDialogIntent(),
+        LogicalKeySet(LogicalKeyboardKey.escape): CloseDialogIntent(),
       },
-      child: currentWidth > 600
-          ?
-          //Desktop
-          Align(
-              alignment: Alignment.topCenter,
-              child: Container(
-                decoration: const BoxDecoration(
-                  color: Color(0xff242526),
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                ),
-                margin: const EdgeInsets.only(top: 80),
-                width: 530,
-                height: 190,
-                child: Column(
-                  children: [
-                    Container(
-                      margin:
-                          const EdgeInsets.only(top: 10, left: 10, right: 10),
-                      child: Material(
-                        color: const Color(0xff242526),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              filled: true,
-                              fillColor: const Color(0xff1b1b1d),
-                              prefixIcon: const Icon(
-                                Icons.search,
-                                color: Color(0xff21b091),
-                              ),
-                              enabledBorder: OutlineInputBorder(
-                                borderSide: const BorderSide(
-                                    width: 3, color: Color(0xff21b091)),
-                                borderRadius: BorderRadius.circular(5),
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                  borderSide: const BorderSide(
-                                      color: Color(0xff21b091), width: 3),
-                                  borderRadius: BorderRadius.circular(5.0)),
-                              hintText: 'Search docs',
-                              hintStyle:
-                                  const TextStyle(color: Colors.white70)),
-                          style: const TextStyle(color: Colors.white),
-                          focusNode: textFieldFocusNode,
-                          autofocus: true,
-                          controller: myController,
-                        ),
-                      ),
+      child: Actions(
+        actions: <Type, Action<Intent>>{
+          CloseDialogIntent:
+              CallbackAction<CloseDialogIntent>(onInvoke: (intent) {
+            Navigator.of(context).maybePop();
+            return null;
+          }),
+        },
+        child: Focus(
+          autofocus: false,
+          child: currentWidth > 600
+              ?
+              //Desktop
+              Align(
+                  alignment: Alignment.topCenter,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      color: Color(0xff242526),
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
                     ),
-                    Container(
-                      color: const Color(0xff242526),
-                      height: 80,
-                      width: 530,
-                      child: const Align(
-                        alignment: Alignment.center,
-                        child: Material(
-                          color: Colors.transparent,
-                          child: Text(
-                            "No recent searches",
-                            style: TextStyle(
-                              color: Colors.white70,
-                              fontSize: 12,
+                    margin: const EdgeInsets.only(top: 80),
+                    width: 530,
+                    height: 190,
+                    child: Column(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(
+                              top: 10, left: 10, right: 10),
+                          child: Material(
+                            color: const Color(0xff242526),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: const Color(0xff1b1b1d),
+                                  prefixIcon: const Icon(
+                                    Icons.search,
+                                    color: Color(0xff21b091),
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: const BorderSide(
+                                        width: 3, color: Color(0xff21b091)),
+                                    borderRadius: BorderRadius.circular(5),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                      borderSide: const BorderSide(
+                                          color: Color(0xff21b091), width: 3),
+                                      borderRadius: BorderRadius.circular(5.0)),
+                                  hintText: 'Search docs',
+                                  hintStyle:
+                                      const TextStyle(color: Colors.white70)),
+                              style: const TextStyle(color: Colors.white),
+                              focusNode: textFieldFocusNode,
+                              autofocus: true,
+                              controller: myController,
                             ),
                           ),
                         ),
-                      ),
-                    ),
-                    Flexible(
-                      child: Container(
-                        decoration: const BoxDecoration(
-                          color: Color(0xff1b1b1d),
-                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                        ),
-                        width: 530,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            )
-          :
-          //Mobile
-          SizedBox(
-              height: double.infinity,
-              width: double.infinity,
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: Column(
-                  children: [
-                    Material(
-                      color: const Color(0xff242526),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8),
-                        child: Row(
-                          children: [
-                            Flexible(
-                              child: TextField(
-                                decoration: InputDecoration(
-                                    filled: true,
-                                    fillColor: const Color(0xff1b1b1d),
-                                    prefixIcon: const Icon(
-                                      Icons.search,
-                                      color: Color(0xff21b091),
-                                    ),
-                                    enabledBorder: OutlineInputBorder(
-                                      borderSide: const BorderSide(
-                                          width: 2, color: Color(0xff21b091)),
-                                      borderRadius: BorderRadius.circular(5),
-                                    ),
-                                    focusedBorder: OutlineInputBorder(
-                                        borderSide: const BorderSide(
-                                            color: Color(0xff21b091), width: 2),
-                                        borderRadius:
-                                            BorderRadius.circular(5.0)),
-                                    hintText: 'Search docs',
-                                    hintStyle:
-                                        const TextStyle(color: Colors.white70)),
-                                style: const TextStyle(color: Colors.white),
-                                focusNode: textFieldFocusNode,
-                                autofocus: true,
-                                controller: myController,
-                              ),
-                            ),
-                            SizedBox(
-                              width: 80,
-                              child: TextButton(
-                                style: TextButton.styleFrom(
-                                  primary: const Color(0xff21b091),
-                                  textStyle: const TextStyle(fontSize: 18),
-                                ),
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                },
-                                child: const Text('Cancel'),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                    Flexible(
-                      child: Container(
-                        color: const Color(0xff242526),
-                        width: currentWidth,
-                        child: const Align(
-                          alignment: Alignment.center,
-                          child: Material(
-                            color: Colors.transparent,
-                            child: Text(
-                              "No recent searches",
-                              style: TextStyle(
+                        Container(
+                          color: const Color(0xff242526),
+                          height: 80,
+                          width: 530,
+                          child: const Align(
+                            alignment: Alignment.center,
+                            child: Material(
+                              color: Colors.transparent,
+                              child: Text(
+                                "No recent searches",
+                                style: TextStyle(
                                   color: Colors.white70,
                                   fontSize: 12,
-                                  decoration: TextDecoration.none),
+                                ),
+                              ),
                             ),
                           ),
                         ),
-                      ),
+                        Flexible(
+                          child: Container(
+                            decoration: const BoxDecoration(
+                              color: Color(0xff1b1b1d),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10.0)),
+                            ),
+                            width: 530,
+                          ),
+                        ),
+                      ],
                     ),
-                    Container(
-                      height: 40,
-                      decoration: const BoxDecoration(
-                        color: Color(0xff1b1b1d),
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                      ),
-                      width: currentWidth,
+                  ),
+                )
+              :
+              //Mobile
+              SizedBox(
+                  height: double.infinity,
+                  width: double.infinity,
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Column(
+                      children: [
+                        Material(
+                          color: const Color(0xff242526),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8),
+                            child: Row(
+                              children: [
+                                Flexible(
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                        filled: true,
+                                        fillColor: const Color(0xff1b1b1d),
+                                        prefixIcon: const Icon(
+                                          Icons.search,
+                                          color: Color(0xff21b091),
+                                        ),
+                                        enabledBorder: OutlineInputBorder(
+                                          borderSide: const BorderSide(
+                                              width: 2,
+                                              color: Color(0xff21b091)),
+                                          borderRadius:
+                                              BorderRadius.circular(5),
+                                        ),
+                                        focusedBorder: OutlineInputBorder(
+                                            borderSide: const BorderSide(
+                                                color: Color(0xff21b091),
+                                                width: 2),
+                                            borderRadius:
+                                                BorderRadius.circular(5.0)),
+                                        hintText: 'Search docs',
+                                        hintStyle: const TextStyle(
+                                            color: Colors.white70)),
+                                    style: const TextStyle(color: Colors.white),
+                                    focusNode: textFieldFocusNode,
+                                    autofocus: true,
+                                    controller: myController,
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 80,
+                                  child: TextButton(
+                                    style: TextButton.styleFrom(
+                                      primary: const Color(0xff21b091),
+                                      textStyle: const TextStyle(fontSize: 18),
+                                    ),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: const Text('Cancel'),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                        Flexible(
+                          child: Container(
+                            color: const Color(0xff242526),
+                            width: currentWidth,
+                            child: const Align(
+                              alignment: Alignment.center,
+                              child: Material(
+                                color: Colors.transparent,
+                                child: Text(
+                                  "No recent searches",
+                                  style: TextStyle(
+                                      color: Colors.white70,
+                                      fontSize: 12,
+                                      decoration: TextDecoration.none),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          height: 40,
+                          decoration: const BoxDecoration(
+                            color: Color(0xff1b1b1d),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.0)),
+                          ),
+                          width: currentWidth,
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
-              ),
-            ),
+        ),
+      ),
     );
   }
 }
